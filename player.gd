46432b9daab550497781aa8f4840121aed4ab5fa
JaydenLester <145381105+JaydenJLester@users.noreply.github.com>
2025-02-25 12:38:32 -0800

    various

--- player.gd
+++ player.gd
@@ -1,25 +1,40 @@
 extends CharacterBody2D
+@onready var sprite_2d = $PlayerSprite
 
 
+#speed should be slow and painful so it feels nice to upgrade
+#TODO: change speed to 175 once done with testing
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	if((velocity.x > 1 || velocity.x < -1 || velocity.y > 1) && !velocity.y < -1):
+		sprite_2d.animation = "walk"
+	elif(velocity.y < -1):
+		sprite_2d.animation = "back"
+	else:
+		sprite_2d.animation = "default"
+	
+	
+	# To Stay looking left or right
+	if Input.is_action_just_pressed('Left'):
+		sprite_2d.flip_h = true
+	if Input.is_action_just_pressed('Right'):
+		sprite_2d.flip_h = false	
+	
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
+	# Handle up and down.
+	var ydirection = Input.get_axis("Up", "Down")
+	if ydirection:
+		velocity.y = ydirection * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, 80)
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = move_toward(velocity.x, 0, 80)
 	move_and_slide()
